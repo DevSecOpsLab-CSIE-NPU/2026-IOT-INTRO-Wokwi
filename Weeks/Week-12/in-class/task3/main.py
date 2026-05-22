fe57d4c3cc14c442c66a6e74e54e69eab8e312d7
1114405022 <ting1011@users.noreply.github.com>
2026-05-22 16:25:52 +0800

    Add solution 1114405022 for Week-13

--- Weeks/Week-12/in-class/task3/main.py
+++ Weeks/Week-12/in-class/task3/main.py
@@ -1,26 +1,22 @@
-from machine import Pin
-import time
+INTERVAL1 = 2000  # 紅 LED 2 秒
+INTERVAL2 = 3000  # 綠 LED 3 秒
 
-led1 = Pin(5, Pin.OUT)  # 紅 LED
-led2 = Pin(2, Pin.OUT)  # 綠 LED
-
-INTERVAL1 = 2000  # ms
-INTERVAL2 = 3000  # ms
-
-t1 = time.ticks_ms()
-t2 = time.ticks_ms()
-s1 = False
-s2 = False
+t1 = t2 = time.ticks_ms()
+s1 = s2 = False
 
 while True:
     now = time.ticks_ms()
 
+    # 紅 LED
     if time.ticks_diff(now, t1) >= INTERVAL1:
         s1 = not s1
         led1.value(s1)
         t1 = now
 
+    # 綠 LED
     if time.ticks_diff(now, t2) >= INTERVAL2:
         s2 = not s2
         led2.value(s2)
         t2 = now
+
+    time.sleep_ms(1)
